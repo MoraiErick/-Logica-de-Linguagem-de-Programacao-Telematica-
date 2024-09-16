9481de1fd2155c3349f9afa0f80d02dbe535c20c
MoraiErick <morais.erick61@aluno.ifce.edu.br>
2024-09-16 19:25:17 -0300

    avaliacao05

--- Avaliacao-05/Codigo.c
+++ Avaliacao-05/Codigo.c
@@ -8,18 +8,16 @@ int main() {
     char linha[MAX_LINE];
     int escolha;
 
-    // Criar arquivo em modo de escrita
     arquivo = fopen("meu_arquivo.txt", "w");
     if (arquivo == NULL) {
         printf("Erro ao criar arquivo!\n");
         return 1;
     }
 
-    // Menu para escolher se vai escrever mais algum texto
     do {
         printf("Digite um texto (ou 'sair' para fechar o arquivo):\n");
         fgets(linha, MAX_LINE, stdin);
-        linha[strcspn(linha, "\n")] = 0; // Remover caractere de quebra de linha
+        linha[strcspn(linha, "\n")] = 0; 
 
         if (strcmp(linha, "sair") != 0) {
             fprintf(arquivo, "%s\n", linha);
@@ -28,25 +26,20 @@ int main() {
         }
     } while (1);
 
-    // Fechar arquivo
     fclose(arquivo);
 
-    // Abrir arquivo em modo de leitura
     arquivo = fopen("meu_arquivo.txt", "r");
     if (arquivo == NULL) {
         printf("Erro ao abrir arquivo!\n");
         return 1;
     }
 
-    // Mostrar cabeçalho
     printf(" \n======== Conteúdo do Arquivo ========\n");
 
-    // Ler cada linha do arquivo
     while (fgets(linha, MAX_LINE, arquivo) != NULL) {
         printf("%s", linha);
     }
 
-    // Fechar arquivo
     fclose(arquivo);
 
     return 0;
