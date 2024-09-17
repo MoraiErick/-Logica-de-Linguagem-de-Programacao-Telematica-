6f88e198dd96cad8e562c11e5dc6de98fe4bdbed
MoraiErick <morais.erick61@aluno.ifce.edu.br>
2024-09-16 22:37:57 -0300

    avaliacao05

--- Avaliacao-05/Codigo.c
+++ Avaliacao-05/Codigo.c
@@ -6,7 +6,6 @@
 int main() {
     FILE *arquivo;
     char linha[MAX_LINE];
-    int escolha;
 
     arquivo = fopen("meu_arquivo.txt", "w");
     if (arquivo == NULL) {
@@ -14,17 +13,16 @@ int main() {
         return 1;
     }
 
-    do {
-        printf("Digite um texto (ou 'sair' para fechar o arquivo):\n");
+    printf("Digite um texto (ou 'sair' para fechar o arquivo):\n");
+    while (1) {
         fgets(linha, MAX_LINE, stdin);
         linha[strcspn(linha, "\n")] = 0; 
-
-        if (strcmp(linha, "sair") != 0) {
-            fprintf(arquivo, "%s\n", linha);
-        } else {
+        if (strcmp(linha, "sair") == 0) {
             break;
         }
-    } while (1);
+
+        fprintf(arquivo, "%s\n", linha);
+    }
 
     fclose(arquivo);
 
